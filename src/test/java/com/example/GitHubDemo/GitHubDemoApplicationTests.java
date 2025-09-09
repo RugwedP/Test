41776964117d779fd23a322b9d6902bbabda7b90
RugwedP <rugwed8181@gmail.com>
2025-09-09 21:17:09 +0530

    project structure

---
+++ src/test/java/com/example/GitHubDemo/GitHubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitHubDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
